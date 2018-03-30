7112e8fe9735657989dabad5cf703a974a3923cc
prajwalx <prajwalbhati@hotmail.com>
2018-03-30 15:38:19 +0530

    Update development.js
        Finding your database connection info
    
    Follow these steps to obtain the information required to connect to your mLab-hosted database(s):
    
    Log in to the mLab management portal.
    Navigate to the MongoDB deployment that you wish to connect to.
    At the top of the screen, you will see a box with the connection information. 
    
    Also create DB User in malls before running app

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,9 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/chat-dev'
+//     uri: 'mongodb://localhost/chat-dev'
+    //http://docs.mlab.com/connecting/
+    uri:  'mongodb://<dbuser>:<dbpwd>@ds135xxx.mlab.com:3xxx/xx'
   },
 
   // Seed database on startup
